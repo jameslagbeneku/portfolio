232066f2e8bf9d9e6eeaae1459794090f28cc3e5
jameslagbeneku <64613959+jameslagbeneku@users.noreply.github.com>
2021-07-13 22:15:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const mobileNav = document.querySelector('.mobile-nav-links')
+const container = document.querySelector('.container')
+const hamburgerBtn = document.querySelector('.hamburger-menu i')
+const mobileCloseBtn = document.querySelector('.mobile-close-menu i')
+const toTopBtn = document.querySelector('.to-top-btn')
+const body = document.querySelector('body')
+
+hamburgerBtn.addEventListener('click', () => {
+    container.style.transform = 'translateX(-100%)';
+    container.style.transition = 'all 0.3s ease-in';
+    mobileNav.style.display = 'block';
+    mobileNav.style.transform = 'translateX(100%)';
+    body.style.overflow = 'hidden'
+    toTopBtn.style.display = 'none'
+})
+
+mobileCloseBtn.addEventListener('click', () => {
+    container.style.transform = 'translateX(0%)';
+    container.style.transition = 'all 0.3s ease-in';
+    mobileNav.style.transform = 'translateX(100%)';
+    body.style.overflowY = 'auto'  
+    toTopBtn.style.display = 'flex' 
+})
